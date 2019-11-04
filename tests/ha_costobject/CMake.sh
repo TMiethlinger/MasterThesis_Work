9c7baf1280d8a23f47d2ecc57dcf8f6bbaa44ca5
ThomasMiethlinger <thomas.miethlinger@gmail.com>
2019-11-04 14:30:17 +0100

    Changed test name according to class name.

--- tests/ha_costobject/CMake.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -rf build
-mkdir build
-cd build
-cmake -DBoost_INCLUDE_DIR="/home/k3501/k354524/apps/boost_1_71_0/" -DBoost_LIBRARY_DIR="/home/k3501/k354524/apps/boost_1_71_0/stage/lib/" ..
-cd ..
--- tests/ha_costobject/Run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd build
-
-N=6
-./ha_costobject_test --N=$N --dim=6
-
